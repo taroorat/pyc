0acae0327e9239ce6dfa93f424bcca96817a200d
Taro <2548316823@qq.com>
2020-12-14 16:54:51 +0800

    图片原始信息json格式

--- main.py
+++ main.py
@@ -5,43 +5,38 @@ from numba import jit
 import cv2 
 import math
 
-@jit
-def run():
-    p = 0
-    for i in range(w):
-        for j in range(h):
-            for k in range(l):
-                p = p+i+j-k
-    print(p)
-
 def go_run():
     client = requests.session()
     headers = {'Content-Type': 'application/json', 'Connection': 'keep-alive'}
 
     response = client.post(url='http://127.0.0.1:31173/cimg', 
                             headers=headers,
-                            data=json.dumps(cImg))
+                            data=json.dumps(src_img_info))
     print(response.text)
     client.close()
 
 
 if __name__ == "__main__":
-    w = 1080
-    h = 1920
-    l = 10
 
-    cImg ={
-        "Width":w,
-        "Height":h,
-        "Length":l
+    img_name = "./images/pikachu001.jpg"
+    common_bgr_color ={'green':[0,255,0],
+                      'red':[0,0,255],
+                      'blue':[255,0,0],
+                      'orange':[0,165,255],
+                      'yellow':[0,255,255],
+                      'cyan':[255,127,0],
+                      'purple':[255,0,139],
+                      'faceA':[109,169,255],
+                      'black':[0,0,0],
+                      'white':[255,255,255],
+    }
+    src_img_info ={
+        "img_name": img_name,
+        "common_bgr_color": common_bgr_color,
     }
 
     start_time = time.time()
-    run()
-    end_time = time.time()
-    print("python",end_time - start_time)
-
-    start_time1 = time.time()
     go_run()
-    end_time1 = time.time()
-    print("go",end_time1 - start_time1)
+    # print(json.dumps(src_img_info))
+    end_time = time.time()
+    print("go",end_time - start_time)
