cc93e437488ddc5546bfc4a66b24e25d96dfd4d1
Taro <2548316823@qq.com>
2020-12-16 17:08:03 +0800

    up

--- main.py
+++ main.py
@@ -16,12 +16,19 @@ def go_run():
 if __name__ == "__main__":
 
     img_name = "d:/GoPath/src/taro.117/gospyc/images/pikachu001.jpg"
-    common_bgr_color = [{"color":"green",
-                        "bgr":[0,255,0]},
-                        {"color":"yellow",
-                        "bgr":[255,0,0]},
-                        {"color":"red",
-                        "bgr":[0,0,255]}]
+    common_bgr_color = [
+                            {"color":"green",
+                            "bgr":[0,255,0]},
+                            {"color":"yellow",
+                            "bgr":[0, 255, 255]},
+                            {"color":"red",
+                            "bgr":[0,0,255]},
+                            {"color":"blue",
+                            "bgr":[255, 0, 0]},
+                            {"color":"orange",
+                            "bgr":[0, 165, 255]},
+                        ]
+
     src_img_info = {
         "img_name": img_name,
         "common_bgr_color": common_bgr_color,
